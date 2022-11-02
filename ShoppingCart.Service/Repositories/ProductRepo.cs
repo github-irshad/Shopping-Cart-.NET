fba0f0e91b844491b7d912895c1bcea7cd012225
irshad_aj <irshadaj08@gmail.com>
2022-11-02 12:42:29 +0530

    Slight change in GetProductById implementation

--- ShoppingCart.Service/Repositories/ProductRepo.cs
+++ ShoppingCart.Service/Repositories/ProductRepo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ShoppingCart.DataAccess.Models;
 using ShoppingCart.Service.Data;
 using ShoppingCart.Service.Infrastructure;
@@ -29,7 +30,7 @@ namespace ShoppingCart.Service.Repositories
 
     public Product GetProductById(int Id)
     {
-      return _context.Products.Where(x=>x.id==Id).FirstOrDefault();
+      return _context.Products.Include(x=>x.Categories).ThenInclude(y=>y.Category).Where(x=>x.id==Id).FirstOrDefault();
     }
 
     public void InsertProduct(Product product)
